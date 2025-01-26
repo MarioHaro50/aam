85adcba358090c7452b0fd33e86ef05c09f38919
Mario Haro <marioharolara@hotmail.com>
2025-01-25 19:11:36 -0600

    name

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,6 +1,6 @@
 import Clients from "../clients/Clients";
 import ContactForm from "../contact/Contact";
-import Header from "../header/Header";
+import Header from "../header/header";
 import Tech from "../tecnologies/Tech";
 import Us from "../us/Us";
 function Body() {
